b202f47cafc687c5461e16cfc41094b27e779730
karla limachi <neleb2442@gmail.com>
2026-04-20 17:38:27 -0400

    port call changes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -22,10 +22,6 @@ const app = express();
 //const PORT = 3000;
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, "0.0.0.0", () => {
-  console.log(`Server running on port ${PORT}`);
-});
-
 // Activamos CORS para permitir peticiones desde el frontend
 app.use(cors());
 
@@ -142,6 +138,6 @@ app.put("/tasks/:id", async (req: any, res: any) => {
   }
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
