a664ce51c5dbb1ea713c0f1b0ea6422d38b92568
Karla Limachi <neleb2442@gmail.com>
2026-07-21 20:01:00 -0400

    fix: ajuste en datos semilla y ajuste en mostrar logs en caso de falló

--- backend/prisma/seed.js
+++ backend/prisma/seed.js
@@ -25,4 +25,8 @@ main()
     console.error(e)
     await prisma.$disconnect()
     process.exit(1)
-  })
+  });
+
+   await prisma.$executeRawUnsafe(
+    `SELECT setval(pg_get_serial_sequence('"Task"', 'id'), COALESCE((SELECT MAX(id) FROM "Task"), 1))`
+  );
