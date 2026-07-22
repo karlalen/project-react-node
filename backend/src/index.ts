b705b14350f1ffce0b76f213de7e3956ae07196c
Karla Limachi <neleb2442@gmail.com>
2026-07-22 00:09:18 -0400

    ajuste

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,8 +3,6 @@ import app from "./app";
 
 const PORT = Number(process.env.PORT) || 3000;
 
-throw new Error('fallo simulado');
-
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
